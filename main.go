91c2da60d58b0e658bfcac11b5381f2b25783c45
Abubakar Sadiq Hassan <abubakarhassan59@gmail.com>
2021-06-05 20:18:29 +0100

    Implemented base route for information purpose

--- main.go
+++ main.go
@@ -1,10 +1,39 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 )
 
+const serviceBasePath = "/"
+
+type ResponseObj struct {
+	Status  bool   `json:"status"`
+	Message string `json:"message"`
+}
+
+func baseHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		response := ResponseObj{true, "Welcome to our Movie Listing Service"}
+
+		js, err := json.Marshal(response)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		_, err = w.Write(js)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
 func main() {
+	http.HandleFunc(serviceBasePath, baseHandler)
 	log.Fatal(http.ListenAndServe(":5300", nil))
 }
